ad60e89abb4813849abb79fc04451c3302bb998e
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-02-13 20:07:41 +0100

    #2

--- Dog.js
+++ Dog.js
@@ -1,1 +1,19 @@
-// Create the Dog class here
+class Dog{
+    constructor(data){
+        Object.assign(this, data)
+    }
+
+    getDogHtml() {
+        const {name, avatar, age, bio} = this
+        return `
+        <img class="dog-img" src="${avatar}" alt="picture of a dog"/>
+            <div class="dog-div">
+                <h1>${name}, ${age}</h1>
+                <p>${bio}</p>
+            </div>
+        `
+    }
+
+}
+
+export default Dog
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Remember to import the data and Dog class!
+import Dog from "./Dog.js"
+import dogs from "./data.js"
+
+const dog = new Dog(dogs[1])
+
+document.getElementById("dog").innerHTML = dog.getDogHtml()
