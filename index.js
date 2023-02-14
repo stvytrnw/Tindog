1ba0947ae37e83479bc84fa04920910f9d140d42
L3RiZ <107563807+L3RiZ@users.noreply.github.com>
2023-02-14 13:57:12 +0100

    #4

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-// Remember to import the data and Dog class!
 import Dog from "./Dog.js"
 import dogs from "./data.js"
 
-const dogArray = dogs.map(dog => )
-console.log(dogArray)
-// let dog = getNewDog();
+const dogArray = dogs.map((dog, i) => i)
+let dog = getNewDog()
 
-// function getNewDog() {
-//    const dogsSwipedArray = dogs[dogArray.shift()];
-//    return dogsSwipedArray ? new Dog(dogsSwipedArray) : {}
-// }
+function getNewDog() {
+   const dogsSwipedArray = dogs[dogArray.shift()]
+   return dogsSwipedArray ? new Dog(dogsSwipedArray) : {}
+}
 
-// document.getElementById("nope-btn").addEventListener('click', () => getNewDog())
-// document.getElementById("yes-btn").addEventListener('click', () => likeDog())
+function likeDog() {
+    document.getElementById('rateLogo').innerHTML = `
+    <img src="images/badge-like.png" alt="like badge" class="likeBadge">`
+    setTimeout(() => swipeDog(), 1500)
+    setTimeout(() => {
+        document.getElementById('rateLogo').innerHTML = ``}, 1500)
+}
 
-// document.getElementById('dog').innerHTML = dog.getDogHtml();
+function dislikeDog() {
+    document.getElementById('rateLogo').innerHTML = `
+    <img src="images/badge-nope.png" alt="dislike badge" class="dislikeBadge">`
+    setTimeout(() => swipeDog(), 1500)
+    setTimeout(() => {
+        document.getElementById('rateLogo').innerHTML = ``}, 1500)
+}
+
+function swipeDog() {
+    dog = getNewDog()
+    render()
+}
+
+function render() {
+    document.getElementById('dog').innerHTML = dog.getDogHtml()
+}
+
+document.getElementById("nope-btn").addEventListener('click', () => dislikeDog())
+document.getElementById("yes-btn").addEventListener('click', () => likeDog())
+
+render()
 
