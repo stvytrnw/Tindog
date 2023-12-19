61a34d897328bf41a7965f1626949c0928fc97a2
L3RiZ <107563807+stvytrnw@users.noreply.github.com>
2023-12-19 15:33:56 +0100

    ds

--- Dog.js
+++ Dog.js
@@ -1,18 +1,18 @@
-class Dog{
-    constructor(data){
-        Object.assign(this, data)
-    }
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
 
-    getDogHtml() {
-        const {name, avatar, age, bio} = this
-        return `
+  getDogHtml() {
+    const { name, avatar, age, bio } = this;
+    return `
         <img class="dog-img" src="${avatar}" alt="picture of a dog"/>
             <div class="dog-div">
                 <h1>${name}, ${age}</h1>
                 <p>${bio}</p>
             </div>
-        `
-    }
+        `;
+  }
 }
 
-export default Dog
+export default Dog;
--- index.js
+++ index.js
@@ -1,53 +1,63 @@
-import Dog from "./Dog.js"
-import dogs from "./data.js"
+import Dog from "./Dog.js";
+import dogs from "./data.js";
 
-const dogArray = dogs.map((dog, i) => i)
-let dog = getNewDog()
+const dogArray = dogs.map((dog, i) => i);
+let dog = getNewDog();
 let isWaiting = false;
 let allDogsSwiped = false;
 
 function getNewDog() {
-   const dogsSwipedArray = dogs[dogArray.shift()]
-   return dogsSwipedArray ? new Dog(dogsSwipedArray) : {}
+  const dogsSwipedArray = dogs[dogArray.shift()];
+  return dogsSwipedArray ? new Dog(dogsSwipedArray) : {};
 }
 
 function likeDog() {
-    dog.hasBeenLiked = true;
-    setRatingBadge();
+  dog.hasBeenLiked = true;
+  setRatingBadge();
 }
 
-function setRatingBadge() {   
-    if (!isWaiting) {
-        isWaiting = true; 
+function setRatingBadge() {
+  if (!isWaiting) {
+    isWaiting = true;
 
-        if (!allDogsSwiped) {
-            dog.hasBeenLiked == true ? 
-            document.getElementById('rateLogo').innerHTML = `<img src="images/badge-like.png" alt="like badge" class="likeBadge">`:
-            document.getElementById('rateLogo').innerHTML = `<img src="images/badge-nope.png" alt="dislike badge" class="dislikeBadge">`
-        }
-       
-
-        setTimeout(() => {
-            document.getElementById('rateLogo').innerHTML = ``
-            isWaiting = false}, 1500)
-        
-        dogArray.length !== 0 ? setTimeout(() => swipeDog(), 1500) :
-        setTimeout(() => {document.getElementById('dog').innerHTML = `<p class="endMessage">You swiped all the dogs!</p>`
-        allDogsSwiped = true}, 1500)
+    if (!allDogsSwiped) {
+      dog.hasBeenLiked == true
+        ? (document.getElementById(
+            "rateLogo"
+          ).innerHTML = `<img src="images/badge-like.png" alt="like badge" class="likeBadge">`)
+        : (document.getElementById(
+            "rateLogo"
+          ).innerHTML = `<img src="images/badge-nope.png" alt="dislike badge" class="dislikeBadge">`);
     }
+
+    setTimeout(() => {
+      document.getElementById("rateLogo").innerHTML = ``;
+      isWaiting = false;
+    }, 1500);
+
+    dogArray.length !== 0
+      ? setTimeout(() => swipeDog(), 1500)
+      : setTimeout(() => {
+          document.getElementById(
+            "dog"
+          ).innerHTML = `<p class="endMessage">You swiped all the dogs!</p>`;
+          allDogsSwiped = true;
+        }, 1500);
+  }
 }
 
 function swipeDog() {
-    dog = getNewDog()
-    render()
+  dog = getNewDog();
+  render();
 }
 
 function render() {
-    document.getElementById('dog').innerHTML = dog.getDogHtml()
+  document.getElementById("dog").innerHTML = dog.getDogHtml();
 }
 
-document.getElementById("nope-btn").addEventListener('click', () => setRatingBadge())
-document.getElementById("yes-btn").addEventListener('click', () => likeDog())
+document
+  .getElementById("nope-btn")
+  .addEventListener("click", () => setRatingBadge());
+document.getElementById("yes-btn").addEventListener("click", () => likeDog());
 
-render()
-
+render();
